4471e5ad9a9cbe2adda8273a71d6ca84e6f95e5c
AhmedKhalil777 <progeng_ahmed_kahlil@outlook.com>
2019-12-19 18:20:03 +0200

    20191219{The Dotnet Core 3 Starting}

---
+++ Dotnet Core 3/HelloCrafts/HelloCrafts/Models/Posts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HelloCrafts.Models
+{
+    public class Posts
+    {
+        public int UID { get; set; }
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Posts>(this);
+    }
+}
