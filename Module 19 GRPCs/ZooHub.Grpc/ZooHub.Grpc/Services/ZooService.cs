d7a97b2f3d433197cb92fd201786060d8fb6cb2f
Ahmed Khalil <Progeng_ahmed_khalil@outlook.com>
2022-05-02 09:01:48 +0200

    Grpc some samples

---
+++ Module 19 GRPCs/ZooHub.Grpc/ZooHub.Grpc/Services/ZooService.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+
+using ZooHub.Grpc;
+namespace ZooHub.Grpc.Services
+{
+    public class ZooService : Zoo.ZooBase
+    {
+        private readonly ILogger<ZooService> _logger;
+
+        public ZooService(ILogger<ZooService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AnimalResponse> GetZooAnimals(ZooRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Sending Animals");
+            var response = new AnimalResponse();
+            // Deadlines
+            // Task.Delay(4000).Wait();
+            response.Animals.AddRange(new List<Animal>{
+                new Animal
+            {
+                Class = "Dog",
+                Id =1,
+                Name="Roy"
+            },
+                new Animal
+                {
+                    Id = 2,
+                    Class="Cat",
+                    Name = "SOSO"
+                }
+            });
+            return Task.FromResult(response);
+        }
+    }
+}
