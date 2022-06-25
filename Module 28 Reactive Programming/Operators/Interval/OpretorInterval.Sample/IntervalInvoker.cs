295c642fa36fe5d66498a991bf449272bb4c92a5
AhmedKhalilIS <ahmed.khalil@iskraemeco.com>
2022-06-25 13:03:02 +0200

    WIP

---
+++ Module 28 Reactive Programming/Operators/Interval/OpretorInterval.Sample/IntervalInvoker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpretorInterval.Sample
+{
+    public class IntervalInvoker<T>
+    {
+        private TimeSpan _waitFor;
+        private Action<List<T>> _action;
+        public List<T> List { get; set; }
+        public IntervalInvoker(double seconds, Action<List<T>> action)
+        {
+            List = new List<T>();
+            _waitFor = TimeSpan.FromSeconds(seconds);
+            _action = action;
+        }
+
+        public void Invoke()
+        {
+            Observable.Interval(_waitFor)
+                .Subscribe(i => {
+                    if (List.Any())
+                    {
+                        _action.Invoke(List);
+                    }
+                    List = new List<T>();
+                });
+        }
+    }
+}
--- Module 28 Reactive Programming/Operators/Interval/OpretorInterval.Sample/Program.cs
+++ Module 28 Reactive Programming/Operators/Interval/OpretorInterval.Sample/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OpretorInterval.Sample;
+
 Console.WriteLine("Hello, World!");
+
+var intervalInvoker = new IntervalInvoker<string>(10,Process);
+
+intervalInvoker.Invoke();
+for (int i = 0; i < 300; i++)
+{
+    intervalInvoker.List.Add(i + "- Hello i am invoked");
+    Thread.Sleep(1000);
+}
+
+Console.ReadKey();
+static void Process(List<string> data)
+{
+    foreach (var dataItem in data)
+    {
+        Console.WriteLine(dataItem);
+    }
+}
