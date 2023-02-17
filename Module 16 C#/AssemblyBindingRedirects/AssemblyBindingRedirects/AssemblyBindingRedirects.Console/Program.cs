5e76724dcce9bc8828ed63c4a94af60b94e8df05
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2023-02-17 12:26:42 +0200

    Assembly Bindings WIP

---
+++ Module 16 C#/AssemblyBindingRedirects/AssemblyBindingRedirects/AssemblyBindingRedirects.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using Sample.Assembly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBindingRedirects.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var smaple = new Sample.Assembly.Sample();
+            smaple.WhoIsMe();
+
+        }
+    }
+}
---
+++ Module 16 C#/AssemblyBindingRedirects/AssemblyBindingRedirects/Sample.Assembly/Sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sample.Assembly
+{
+    public class Sample
+    {
+        public void WhoIsMe()
+        {
+            Console.WriteLine("I am Sample");
+        }
+
+    }
+}
